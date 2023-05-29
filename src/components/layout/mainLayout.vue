d3e3c67e1a8706f5b4431537c23d11f56355e5fe
atippaz <nasakun13201@gmail.com>
2023-05-29 18:10:16 +0700

    update commit

--- src/components/layout/mainLayout.vue
+++ src/components/layout/mainLayout.vue
@@ -1,10 +1,11 @@
 <template>
+  <!-- <HomeView></HomeView> -->
   <RouterView />
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { RouterView } from 'vue-router'
-
+import HomeView from '@/views/HomeView.vue'
 export default defineComponent({
   components: { RouterView }
 })
