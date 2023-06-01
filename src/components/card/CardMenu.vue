efd5c412a16569c0839ae81384820900dba51b27
atippaz <nasakun13201@gmail.com>
2023-06-01 13:31:34 +0700

    refactor

--- src/components/card/CardMenu.vue
+++ src/components/card/CardMenu.vue
@@ -3,11 +3,7 @@
     <slot />
   </div>
 </template>
-<script lang="ts">
-import { defineComponent } from 'vue'
 
-export default defineComponent({})
-</script>
 <style>
 .card {
   border-radius: 8px;
