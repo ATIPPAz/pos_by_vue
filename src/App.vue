a707f4564f131825cb137a8ffa8bfb5d1e4f4730
premSuperdev <108412360+premSuperdev@users.noreply.github.com>
2023-06-02 10:23:23 +0700

    save

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,8 @@
   <ToastNotification />
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import GlobalLoader from '@/components/loader/GlobalLoader.vue'
-import { defineComponent } from 'vue'
 import ToastNotification from '@/components/toast/ToastNotification.vue'
 import { RouterView } from 'vue-router'
-export default defineComponent({
-  components: {
-    RouterView,
-    ToastNotification,
-    GlobalLoader
-  }
-})
 </script>
